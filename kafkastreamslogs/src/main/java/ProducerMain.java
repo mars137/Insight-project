4a798e105abb82220febb442f23cb99302e1d7eb
mars137 <atif.tahir13@gmail.com>
2017-06-13 05:28:19 -0700

    Added comments for kafka producer executor class

--- kafkastreamslogs/src/main/java/ProducerMain.java
+++ kafkastreamslogs/src/main/java/ProducerMain.java
@@ -4,6 +4,7 @@ import util.MyProperties;
 
 /**
  * Created by mars137 on 6/11/17.
+   Producer executor for generating kafka streams for Ad clicks, impressions and site_analytics(conversions)
  */
 public class ProducerMain
 
@@ -11,6 +12,8 @@ public class ProducerMain
 
     public static void main(String[] args)
     {
+        
+        //Properties for initializing the Kafka producer
         MyProperties myProperties1 = new MyProperties();
         myProperties1.loadProperties("src/main/resources/myproducer_clicks.properties");
         Producer<String, String> prod1 = new KafkaProducer<>(myProperties1);
@@ -24,44 +27,24 @@ public class ProducerMain
         Producer<String, String> prod3 = new KafkaProducer<>(myProperties3);
 
 
-
+        //CSV files downloaded from S3 
         String clicks_path = "/home/hadoop-user/Data_sets/clicks_1.csv";
         String impression_path = "/home/hadoop-user/Data_sets/im.csv";
         String site_analytics = "/home/hadoop-user/Data_sets/ste.csv";
+        //Topics for kafka producer
         String topic_clicks= "cltest";
         String topic_impressions ="imtest";
         String topic_site="sitecl";
-
+        
+        //Intitalizing object of MyProducer class containing the code for producer implementation
         MyProducer pr1 = new MyProducer();
         MyProducer pr2 = new MyProducer();
         MyProducer pr3 = new MyProducer();
-
+        
+        //Start the Kafka producer using the above mentioned topics 
         pr1.produce(topic_clicks,clicks_path,prod1);
         pr2.produce(topic_impressions,impression_path,prod2);
         pr3.produce(topic_site,site_analytics,prod3);
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
