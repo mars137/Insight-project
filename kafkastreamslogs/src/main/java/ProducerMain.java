14e89ce38b377d62a9348ed89652732f4f179bf9
mars137 <atif.tahir13@gmail.com>
2017-06-15 11:01:45 -0700

    Modified Producer executor ,included properties config

--- kafkastreamslogs/src/main/java/ProducerMain.java
+++ kafkastreamslogs/src/main/java/ProducerMain.java
@@ -2,9 +2,11 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import util.MyProperties;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by mars137 on 6/11/17.
-   Producer executor for generating kafka streams for Ad clicks, impressions and site_analytics(conversions)
  */
 public class ProducerMain
 
@@ -12,39 +14,63 @@ public class ProducerMain
 
     public static void main(String[] args)
     {
-        
-        //Properties for initializing the Kafka producer
-        MyProperties myProperties1 = new MyProperties();
-        myProperties1.loadProperties("src/main/resources/myproducer_clicks.properties");
-        Producer<String, String> prod1 = new KafkaProducer<>(myProperties1);
+        MyProperties clickProperties = new MyProperties();
+        clickProperties.loadProperties("src/main/resources/myproducer_clicks.properties");
+        Producer<String, String> prod1 = new KafkaProducer<>(clickProperties);
 
-        MyProperties myProperties2 = new MyProperties();
-        myProperties2.loadProperties("src/main/resources/myproducer_im.properties");
-        Producer<String, String> prod2 = new KafkaProducer<>(myProperties2);
+        MyProperties impressionProperties = new MyProperties();
+        impressionProperties.loadProperties("src/main/resources/myproducer_im.properties");
+        Producer<String, String> prod2 = new KafkaProducer<>(impressionProperties);
 
-        MyProperties myProperties3 = new MyProperties();
-        myProperties3.loadProperties("src/main/resources/myproducer_st.properties");
-        Producer<String, String> prod3 = new KafkaProducer<>(myProperties3);
+        MyProperties conversionProperties = new MyProperties();
+        conversionProperties.loadProperties("src/main/resources/myproducer_st.properties");
+        try (Producer<String, String> prod3 = new KafkaProducer<>(conversionProperties)) {
 
 
-        //CSV files downloaded from S3 
-        String clicks_path = "/home/hadoop-user/Data_sets/clicks_1.csv";
-        String impression_path = "/home/hadoop-user/Data_sets/im.csv";
-        String site_analytics = "/home/hadoop-user/Data_sets/ste.csv";
-        //Topics for kafka producer
-        String topic_clicks= "cltest";
-        String topic_impressions ="imtest";
-        String topic_site="sitecl";
-        
-        //Intitalizing object of MyProducer class containing the code for producer implementation
-        MyProducer pr1 = new MyProducer();
-        MyProducer pr2 = new MyProducer();
-        MyProducer pr3 = new MyProducer();
-        
-        //Start the Kafka producer using the above mentioned topics 
-        pr1.produce(topic_clicks,clicks_path,prod1);
-        pr2.produce(topic_impressions,impression_path,prod2);
-        pr3.produce(topic_site,site_analytics,prod3);
+            Properties fileProperties = new Properties();
+            fileProperties.load(ProducerMain.class.getResourceAsStream("/file_config.properties"));
+
+            String clicks_path = fileProperties.getProperty("clicks_path");
+            String impression_path = fileProperties.getProperty("impression_path");
+            String site_path = fileProperties.getProperty("site_clicks");
+
+            String topic_clicks = fileProperties.getProperty("topic_clicks");
+            String topic_impressions = fileProperties.getProperty("topic_impressions");
+            String topic_site = fileProperties.getProperty("topic_site");
+
+            MyProducer pr1 = new MyProducer();
+            MyProducer pr2 = new MyProducer();
+            MyProducer pr3 = new MyProducer();
+
+            pr1.produce(topic_clicks, clicks_path, prod1);
+            pr2.produce(topic_impressions, impression_path, prod2);
+            pr3.produce(topic_site, site_path, prod3);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
